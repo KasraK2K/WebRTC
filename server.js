71c89133ff88c22987a25de6e19f88a6ff20f232
Kasra <Kasra_K2K@yahoo.com>
2020-09-11 23:25:41 +0430

    simple start with WebRTC

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// express config
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+// routes
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server started on port ${port}`));
