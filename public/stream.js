01f43ca5e7b92f07cee4eee16aa080121b38659c
Kasra Karami <30779339+KasraK2K@users.noreply.github.com>
2020-09-13 13:22:22 +0430

    take a picture

--- public/stream.js
+++ public/stream.js
@@ -1,5 +1,55 @@
 var videoArea = document.querySelector('video');
 var videoSelect = document.querySelector('#camera');
+var profilePicCanvas = document.querySelector('#profilePicCanvas');
+var profilePictureOutput = document.querySelector('#profilePictureOutput');
+var takeProfilePicture = document.querySelector('#takeProfilePicture');
+var videoTag = document.querySelector('#videoTag');
+
+takeProfilePicture.addEventListener(
+  'click',
+  function (event) {
+    takeProfilePic();
+    event.preventDefault();
+  },
+  false,
+);
+
+var width = 240; // Desire width of the profile picture
+var height = 0; // Calculated later base on image retio
+var streaming = false; // Useto determine when the video has loaded
+
+videoTag.addEventListener(
+  'canplay',
+  function (event) {
+    if (!streaming) {
+      height = videoTag.videoHeight / (videoTag.videoWidth / width);
+
+      if (isNaN(height)) {
+        height = width / (4 / 3);
+      }
+
+      videoTag.setAttribute('width', width);
+      videoTag.setAttribute('height', height);
+      profilePicCanvas.setAttribute('width', width);
+      profilePicCanvas.setAttribute('height', height);
+      streaming = true;
+    }
+  },
+  false,
+);
+
+// take a picture from videoTag
+function takeProfilePic() {
+  var context = profilePicCanvas.getContext('2d');
+  if (width && height) {
+    profilePicCanvas.width = width;
+    profilePicCanvas.height = height;
+    context.drawImage(videoTag, 0, 0, width, height);
+
+    var data = profilePicCanvas.toDataURL('image/png');
+    profilePictureOutput.setAttribute('src', data);
+  }
+}
 
 // get video sources and create select source
 navigator.mediaDevices
@@ -35,8 +85,8 @@ function startStream() {
     audio: false,
     video: {
       mandatory: {
-        width: { min: 400, ideal: 640, max: 800 },
-        height: { min: 300, ideal: 480, max: 600 },
+        width: { min: 240, ideal: 240, max: 240 },
+        height: { min: 240, ideal: 240, max: 240 },
       },
       deviceId: videoSource,
     },
