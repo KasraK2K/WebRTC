4bd4e81323cd355e295604a8270f9a0c54e96da5
Kasra Karami <Kasra_K2K@yahoo.com>
2020-09-22 13:14:09 +0330

    remove take picture

--- public/stream.js
+++ public/stream.js
@@ -1,55 +1,5 @@
 var videoArea = document.querySelector('video');
 var videoSelect = document.querySelector('#camera');
-var profilePicCanvas = document.querySelector('#profilePicCanvas');
-var profilePictureOutput = document.querySelector('#profilePictureOutput');
-var takeProfilePicture = document.querySelector('#takeProfilePicture');
-var videoTag = document.querySelector('#videoTag');
-
-takeProfilePicture.addEventListener(
-  'click',
-  function (event) {
-    takeProfilePic();
-    event.preventDefault();
-  },
-  false,
-);
-
-var width = 800; // Desire width of the profile picture
-var height = 0; // Calculated later base on image retio
-var streaming = false; // Use to determine when the video has loaded
-
-videoTag.addEventListener(
-  'canplay',
-  function (event) {
-    if (!streaming) {
-      height = videoTag.videoHeight / (videoTag.videoWidth / width);
-
-      if (isNaN(height)) {
-        height = width / (4 / 3);
-      }
-
-      videoTag.setAttribute('width', width);
-      videoTag.setAttribute('height', height);
-      profilePicCanvas.setAttribute('width', width);
-      profilePicCanvas.setAttribute('height', height);
-      streaming = true;
-    }
-  },
-  false,
-);
-
-// take a picture from videoTag
-function takeProfilePic() {
-  var context = profilePicCanvas.getContext('2d');
-  if (width && height) {
-    profilePicCanvas.width = width;
-    profilePicCanvas.height = height;
-    context.drawImage(videoTag, 0, 0, width, height);
-
-    var data = profilePicCanvas.toDataURL('image/png');
-    profilePictureOutput.setAttribute('src', data);
-  }
-}
 
 // get video sources and create select source
 navigator.mediaDevices
@@ -83,13 +33,14 @@ function startStream() {
   var videoSource = videoSelect.value;
   var constrains = {
     audio: false,
-    video: {
-      mandatory: {
-        width: { min: 240, ideal: 240, max: 240 },
-        height: { min: 240, ideal: 240, max: 240 },
-      },
-      deviceId: videoSource,
-    },
+    video: false,
+    // video: {
+    //   mandatory: {
+    //     width: { min: 240, ideal: 240, max: 240 },
+    //     height: { min: 240, ideal: 240, max: 240 },
+    //   },
+    //   deviceId: videoSource,
+    // },
   };
   navigator.getUserMedia(constrains, onSuccess, onError);
 }
