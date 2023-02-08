af4490be57ede3ab6fc7d54de7d856585fa6fcb3
Kasra Karami <Kasra_K2K@yahoo.com>
2023-02-08 23:56:48 +0330

    tide up

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const morgan = require('morgan')
 
 // express config
-app.use(morgan('dev'));
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(morgan('dev'))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 // routes
 app.get('/', function (req, res) {
-  res.render('index');
-});
+  res.render('index')
+})
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`server started on port ${port}`));
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`server started on port ${port}`))
